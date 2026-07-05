cc797f15eaff7e3f769734d7a2db7afb31e843c9
0xCosmix <luca092496.25@gmail.com>
2026-07-05 09:01:13 +0200

    Initial commit

---
+++ src/main/java/net/cosmix/tracelab/TraceLabClient.java
@@ -0,0 +1,10 @@
+package net.cosmix.tracelab;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TraceLabClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
